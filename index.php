6e8092617319db3021f200286f24d99c473c6d20
Oswall Freitez <ofreitez13@gmail.com>
2024-09-22 20:06:48 -0300

    probando rama

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Strager things</title>
+    <title>Strager things 2</title>
 </head>
 
 <body>
