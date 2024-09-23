f89dd7286ea7685b39739672bda72a35e143332d
Oswall Freitez <ofreitez13@gmail.com>
2024-09-22 22:48:55 -0300

    ajuste de home page

--- index.php
+++ index.php
@@ -5,40 +5,73 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="assets/css/estilos.css">
-    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
-    
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        fondo: '#080c27',
+                    }
+                }
+            }
+        }
+    </script>
+
     <title>Inicio | Stranger Things</title>
 </head>
 
 <body>
-    <header>
-        <div id="logo">
-            <a href="index.php">
-                <img src="assets/imagenes/Stranger World.png" alt="logo">
+    <header class="bg-black">
+        <div class="container mx-auto flex justify-between align-center py-4">
+            <a class="flex align-center" href="index.php">
+                <img class="h-10 my-auto py-1 border-t border-b border-red-600" src="assets/imagenes/Stranger World.png" alt="logo">
             </a>
+
+            <nav class="my-auto font-montserrat">
+                <ul class="flex text-white gap-4">
+                    <li>
+                        <a href="temporadas.php">Temporadas</a>
+                    </li>
+                    <li>
+                        <a href="personajes.php">Personajes</a>
+                    </li>
+                    <li>
+                        <a href="fanart.php">Fan art</a>
+                    </li>
+
+                </ul>
+            </nav>
         </div>
-
-        <nav class="menu">
-            <ul>
-                <li>
-                    <a href="temporadas.php">Temporadas</a>
-                </li>
-                <li>
-                    <a href="personajes.php">Personajes</a>
-                </li>
-                <li>
-                    <a href="fanart.php">Fan art</a>
-                </li>
-
-            </ul>
-        </nav>
     </header>
-    <main class="pagina-principal">
-        <section class="contenedor-principal">
-            <div class="contenido-principal">
-                <div class="texto-principal">
-                    <h1>
+    <main class="">
+        <section class="hero-background">
+            <div class="container mx-auto pt-12 pb-96 grid grid-cols-2 gap-4">
+                <div></div>
+                <div class="flex flex-col gap-4">
+                    <h1 class="font-bold mb-2 text-3xl">
+                        Descubre el otro lado de Hawkins
+                    </h1>
+                    <p class="mb-8">
+                        Stranger Things es una serie de televisión web estadounidense de suspenso y ciencia ficción
+                        coproducida y distribuida por Netflix.Escrita y dirigida por los hermanos Matt y Ross Duffer, y
+                        producida ejecutivamente por Shawn Levy,se estrenó en la plataforma Netflix el 15 de julio de
+                        2016.
+                    </p>
+                    <div class="mx-auto">
+                        <a href="#contacto" class="rounded-3xl bg-red-600 font-bold py-2 px-12">
+                            Contactanos
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <section class="container mx-auto py-12 grid grid-cols-2 gap-4">
+            <div class="">
+                <div class="">
+                    <h1 class="font-bold mb-2 text-xl">
                         Stranger Things
                     </h1>
                     <p>
@@ -59,40 +92,40 @@
 
                 </div>
             </div>
-            <div class="video-principal">
-                <iframe src="https://www.youtube.com/embed/-RcPZdihrp4?si=C7LGOg4ow5ENKlYc" title="YouTube video player"
+            <div class="flex w-full">
+                <iframe class="w-full h-80 rounded-xl border border-red-600" src="https://www.youtube.com/embed/-RcPZdihrp4?si=C7LGOg4ow5ENKlYc" title="YouTube video player"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     allowfullscreen></iframe>
             </div>
         </section>
 
-        <section class="top-peli-contenedor">
-            <h1>Galeria de arte - Fans</h1>
-            <ul class="top-peli-lista">
-                <li>
+        <section class="container mx-auto py-12">
+            <h1 class="font-bold mb-8 text-xl text-center">Galeria de arte - Fans</h1>
+            <ul class="grid grid-cols-3 gap-8">
+                <li class="text-center mx-auto">
                     <h2>#1: Anonimo</h2>
-                    <figure class="top-peli-imagen">
-                        <img src="assets/imagenes/strangerthingsinicio1.png" alt="Art Gallery 1">
+                    <figure class="">
+                        <img class="w-52" src="assets/imagenes/strangerthingsinicio1.png" alt="Art Gallery 1">
                     </figure>
                 </li>
-                <li>
+                <li class="text-center mx-auto">
                     <h2>#2: Anonimo</h2>
-                    <figure class="top-peli-imagen">
-                        <img src="assets/imagenes/strangerthingsinicio2.jpg" alt="Art Gallery 2">
+                    <figure class="">
+                        <img class="w-52" src="assets/imagenes/strangerthingsinicio2.jpg" alt="Art Gallery 2">
                     </figure>
                 </li>
-                <li>
+                <li class="text-center mx-auto">
                     <h2>#3: Anonimo</h2>
-                    <figure class="top-peli-imagen">
-                        <img src="assets/imagenes/strangerthingsinicio3.png" alt="Art Gallery 3">
+                    <figure class="">
+                        <img class="w-52" src="assets/imagenes/strangerthingsinicio3.png" alt="Art Gallery 3">
                     </figure>
                 </li>
             </ul>
         </section>
-        <section class="container mx-auto mb-8">
-            <h1 class="mb-2 text-lg text-left">Contactanos!</h1>
-            <form class="text-white flex flex-col gap-4 rounded-lg border border-red-600 p-6" action="contacto/guardar_contacto.php" method="POST">
+        <section id="contacto" class="container mx-auto py-12">
+            <h1 class="mb-2 text-xl text-center font-bold">Contactanos!</h1>
+            <form class="bg-black mx-auto w-[580px] text-white flex flex-col gap-4 rounded-lg border border-red-600 p-6" action="contacto/guardar_contacto.php" method="POST">
                 <div class="flex flex-col mb-2">
                     <label for="nombre">
                         Nombre
@@ -117,34 +150,36 @@
                 </div>
             </form>
         </section>
-        <button class="scroll-arriba">
+        <button class="border border-red-600 shadow-md shadow-red-600 fixed bottom-4 right-4 py-3 px-5 rounded-lg">
             <a href="#">
                 ⇧
             </a>
         </button>
     </main>
-    <footer class="border-t-red-600 border-t-2">
-        <div class="container mx-auto justify-evenly w-full flex mt-6 mb-8">
+    <footer class="py-8 bg-black border-t-red-600 border-t-2">
+        <div class="container mx-auto ">
 
-            <img class="h-6" src="assets/imagenes/Stranger World.png" alt="logo">
-            <nav>
-                <ul class="flex gap-2">
-                    <li> Información </li>
-                    <li> Términos y Condiciones </li>
-                    <li> Política de Privacidad </li>
-                </ul>
-            </nav>
-            <div class="text-left">
-                <h3 class="text-left mb-2">Redes sociales</h3>
-                <div class="flex gap-2">
-                    <img class="h-5" src="assets/imagenes/facebook_logo.svg" alt="logo">
-                    <img class="h-5" src="assets/imagenes/instagram_logo.png" alt="logo">
-                    <img class="h-5" src="assets/imagenes/youtube_logo.png" alt="logo">
+            <div class="flex justify-evenly w-full mb-8">
+                <img class="h-6" src="assets/imagenes/Stranger World.png" alt="logo">
+                <nav>
+                    <ul class="flex gap-2">
+                        <li> Información </li>
+                        <li> Términos y Condiciones </li>
+                        <li> Política de Privacidad </li>
+                    </ul>
+                </nav>
+                <div class="text-left">
+                    <h3 class="text-left mb-2">Redes sociales</h3>
+                    <div class="flex gap-2">
+                        <img class="h-5" src="assets/imagenes/facebook_logo.svg" alt="logo">
+                        <img class="h-5" src="assets/imagenes/instagram_logo.png" alt="logo">
+                        <img class="h-5" src="assets/imagenes/youtube_logo.png" alt="logo">
+                    </div>
                 </div>
             </div>
-        </div>
-        <div>
-            <p class="text-xs">Stranger Things © 2024 | Creado por Oswall Freitez</p>
+            <div class="text-center">
+                <p class="text-xs">Stranger Things © 2024 | Creado por Oswall Freitez</p>
+            </div>
         </div>
     </footer>
 </body>
