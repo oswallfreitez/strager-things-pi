2688b77c7725c7aef8b4dd3644b212466df5bee9
Oswall Freitez <ofreitez13@gmail.com>
2024-12-03 17:55:04 -0300

    agregado menu y footer a paginas registrarse y login. ajuste estilos y fondo

--- register.php
+++ register.php
@@ -24,13 +24,58 @@
 </head>
 
 <body>
+    <header class="bg-black">
+        <div class="container mx-auto flex justify-between align-center py-4">
+            <a class="flex align-center" href="index.php">
+                <img class="h-10 my-auto py-1 border-t border-b border-red-600" src="assets/imagenes/Stranger World.png" alt="logo">
+            </a>
+
+            <nav class="my-auto font-montserrat">
+                <ul class="flex text-white gap-4">
+                    <li>
+                        <a href="la-serie.php">La Serie</a>
+                    </li>
+                    <li>
+                        <a href="temporadas.php">Temporadas</a>
+                    </li>
+                    <li>
+                        <a href="personajes.php">Personajes</a>
+                    </li>
+                    <li id="cta-fanArt" class="hidden">
+                        <a href="fanart.php">Fan art</a>
+                    </li>
+                    <li id="cta-registrarse">
+                        <a href="register.php" class="rounded-3xl bg-transparent border-red-600 border-2 font-bold py-2 px-8">Registrarse</a>
+                    </li>
+                    <li id="cta-login">
+                        <a href="login.php" class="rounded-3xl bg-red-600 font-bold py-2 px-8">Login</a>
+                    </li>
+                    <li class="font-light">
+                        |
+                    </li>
+                    <li id="cta-bienvenido" class="hidden font-light">
+                    </li>
+                    <li id="cta-logout" class="hidden">
+                        <a href="#" class="rounded-3xl bg-red-600 font-bold py-2 px-8">Logout</a>
+                    </li>
+                    <li class="flex items-center">
+                        <a href="busqueda.php">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#ffffff" height="16px" width="16px" version="1.1" id="Capa_1" viewBox="0 0 490.4 490.4" xml:space="preserve">
+                                <g>
+                                    <path d="M484.1,454.796l-110.5-110.6c29.8-36.3,47.6-82.8,47.6-133.4c0-116.3-94.3-210.6-210.6-210.6S0,94.496,0,210.796   s94.3,210.6,210.6,210.6c50.8,0,97.4-18,133.8-48l110.5,110.5c12.9,11.8,25,4.2,29.2,0C492.5,475.596,492.5,463.096,484.1,454.796z    M41.1,210.796c0-93.6,75.9-169.5,169.5-169.5s169.6,75.9,169.6,169.5s-75.9,169.5-169.5,169.5S41.1,304.396,41.1,210.796z" />
+                                </g>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
     <main>
-        <div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 fondo-noche-roja">
             <div class="sm:mx-auto sm:w-full sm:max-w-md">
-                <a class="flex justify-center align-center" href="index.php">
-                    <img class="h-12 my-auto py-1 border-t border-b border-red-600" src="assets/imagenes/Stranger World.png" alt="logo">
-                </a>
-                <h2 class="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-white">Únete a la comunidad Stranger</h2>
+                <h1 class="mt-6 text-center text-4xl font-bold leading-9 tracking-tight text-white">Únete a la comunidad Stranger!</h1>
             </div>
 
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
@@ -104,6 +149,34 @@
             </div>
         </div>
     </main>
+
+    <footer class="py-8 bg-black border-t-red-600 border-t-2">
+        <div class="container mx-auto ">
+
+            <div class="flex justify-evenly w-full mb-8">
+                <img class="h-6" src="assets/imagenes/Stranger World.png" alt="logo">
+                <nav>
+                    <ul class="flex gap-2">
+                        <li> Información </li>
+                        <li> Términos y Condiciones </li>
+                        <li> Política de Privacidad </li>
+                    </ul>
+                </nav>
+                <div class="text-left">
+                    <h3 class="text-left mb-2">Redes sociales</h3>
+                    <div class="flex gap-2">
+                        <img class="h-5" src="assets/imagenes/facebook_logo.svg" alt="logo">
+                        <img class="h-5" src="assets/imagenes/instagram_logo.png" alt="logo">
+                        <img class="h-5" src="assets/imagenes/youtube_logo.png" alt="logo">
+                    </div>
+                </div>
+            </div>
+            <div class="text-center">
+                <p class="text-xs">Stranger Things © 2024 | Creado por Oswall Freitez</p>
+            </div>
+        </div>
+    </footer>
+
     <script src="assets/js/registrarse.js"></script>
 
 </body>
